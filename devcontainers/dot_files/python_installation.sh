0afd1fe3f5c3d4333038e09efcedc14ebe0176e5
Isra-ResVo <israelrescalvo@outlook.com>
2023-01-18 10:55:52 -0600

    elimiate the apt-upgrade from the script

--- devcontainers/dot_files/python_installation.sh
+++ devcontainers/dot_files/python_installation.sh
@@ -1,45 +1,66 @@
 #!/bin/bash
 
 #updating
-apt update
-apt upgrade
+apt-get update
 
 # installing curl if it doesn't exist
-type -p curl > /dev/null || apt install -y curl
-type -p git > /dev/null || apt install -y git
+type -p curl >/dev/null || apt-get install -y curl
+type -p git >/dev/null || apt-get install -y git
 
 # Creating bashrc if doesn't exist
 test -f ~/.bashrc || touch ~/.bashrc
 
-# Installing c compiler
-type -p gcc > /dev/null || apt install -y build-essential
+# to avoid intereactive request when intallng packages
+export DEBIAN_FRONTEND=noninteractive
+
+# Installing all the necessary tools to build python
+apt-get install -y build-essential --no-install-recommends make \
+    build-essential \
+    libssl-dev \
+    zlib1g-dev \
+    libbz2-dev \
+    libreadline-dev \
+    libsqlite3-dev \
+    wget \
+    curl \
+    llvm \
+    libncurses5-dev \
+    xz-utils tk-dev \
+    libxml2-dev \
+    libxmlsec1-dev \
+    libffi-dev \
+    liblzma-dev
 
 #--------------------------------------pyenv-----------------------------------
 # Saving the dir of this file
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-# The  nextsection will crate a temp file to make a copy of bashrc to try to append
-# a little section of code to help to pyenv run when bash is started.
-# If for some reason this script fail the trap command will delete it. And 
-# aditionally a temp file must be eliminated for security reasons.
-trap 'rm -f "$TMPFILE"' EXIT 
-TMPFILE=$(mktemp) || exit 1 
+# The  nextsection will crate a temp file to make a copy of bashrc to
+# try to append a little section of code to help to pyenv run when bash
+# is started. If for some reason this script fail the trap command will
+# delete it. And aditionally a temp file must be eliminated for security
+# reasons.
+
+trap 'rm -f "$TMPFILE"' EXIT
+TMPFILE=$(mktemp) || exit 1
 
 # Saving a backup
-cat "$HOME/.bashrc" >| "$HOME/.pyenv_bashrc_backup"
+cat "$HOME/.bashrc" >|"$HOME/.pyenv_bashrc_backup"
 # A file cannnot be part of the same opeartion for that reason is
 # used a temp file
-cat "$SCRIPT_DIR/pyenv_config" "$HOME/.bashrc" >| "$TMPFILE"
-cat  "$TMPFILE"  >| "$HOME/.bashrc"
+cat "$SCRIPT_DIR/pyenv_config" "$HOME/.bashrc" >|"$TMPFILE"
+cat "$TMPFILE" >|"$HOME/.bashrc"
 
 # installing pyenv and python version
 curl https://pyenv.run | bash
 source "$HOME/.bashrc"
+
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/shims:${PYENV_ROOT}/bin:$PATH"
+
 pyenv install 3.10
 pyenv global 3.10
 
-#add an alias to python3 to ,bashrc
-echo 'alias python="python3"' >> "$HOME/.bashrc"
-
 # --------------------------- installing poetry ---------------------------------
-# curl -sSL https://install.python-poetry.org | python3 -
+# reviewing if the command change with the name
+curl -sSL https://install.python-poetry.org | python3 - || curl -sSL https://install.python-poetry.org | python -
